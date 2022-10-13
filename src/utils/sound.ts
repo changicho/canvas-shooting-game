df54a1aa87cce7bf7300a58d497ae244bd7f2066
Changi Cho <eunuch74@gmail.com>
2022-10-13 23:26:09 +0900

    :sparkles: Create sound object

---
+++ src/utils/sound.ts
@@ -0,0 +1,6 @@
+export const introMusic = new Audio("@/assets/music/introSong.mp3");
+export const shootingSound = new Audio("@/assets/music/shoooting.mp3");
+export const killEnemySound = new Audio("@/assets/music/killEnemy.mp3");
+export const gameOverSound = new Audio("@/assets/music/gameOver.mp3");
+export const heavyWeaponSound = new Audio("@/assets/music/heavyWeapon.mp3");
+export const hugeWeaponSound = new Audio("@/assets/music/hugeWeapon.mp3");
